69d16d432be46e736c2a7df5a9eee24406116218
Mike Solomon <mike120982@gmail.com>
2022-11-27 10:04:05 +0200

    Runs install script in project dir

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ fs.writeFileSync(
 // the dependencies. We are using a third-party library
 // called `cross-spawn` for cross-platform support.
 // (Node has issues spawning child processes in Windows).
-spawn.sync('npm', ['install'], { stdio: 'inherit' });
+spawn.sync('npm', ['install'], { cwd: projectDir, stdio: 'inherit' });
 
 console.log('Success! Your new project is ready.');
 console.log(`Created ${projectName} at ${projectDir}`);
